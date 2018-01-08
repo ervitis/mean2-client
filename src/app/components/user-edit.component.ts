e4e320a1aca4d70059e2722d41fac4f83a3c862e
ervitis <vitomarti@gmail.com>
2018-01-08 09:58:12 +0100

    feat: upload picture user

--- src/app/components/user-edit.component.ts
+++ src/app/components/user-edit.component.ts
@@ -1,6 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from '../services/user.service';
 import {User} from '../models/user';
+import {GLOBAL} from '../services/global';
+
+const READY_STATE = 4;
+const HTTP_OK = 200;
 
 @Component({
     selector: 'user-edit',
@@ -14,6 +18,7 @@ export class UserEditComponent implements OnInit {
     public identity;
     public token;
     public alertMessage;
+    public filesToUpload: Array<File>;
 
     constructor(private userService: UserService) {
         this.titulo = 'Update user data';
@@ -40,10 +45,25 @@ export class UserEditComponent implements OnInit {
                 this.user = res['user'] != null ? res['user'] : null;
 
                 if (! this.user) {
-                    this.alertMessage = 'User not updated'
+                    this.alertMessage = 'User not updated';
                 } else {
                     localStorage.setItem('identity', JSON.stringify(this.user));
-                    this.alertMessage = 'User data updated'
+                    this.alertMessage = 'User data updated';
+
+                    if (this.filesToUpload) {
+                        this.makeFileRequest(
+                            GLOBAL.url + 'user/photo/' + this.user._id,
+                            [],
+                            this.filesToUpload
+                        ).then((result: any) => {
+                            console.log(result);
+                            this.user.image = result.image;
+                            localStorage.setItem('identity', JSON.stringify(this.user));
+                            console.log(this.user);
+                        }).catch(error => {
+                            console.log(error);
+                        });
+                    }
                 }
             },
             error => {
@@ -52,6 +72,36 @@ export class UserEditComponent implements OnInit {
                 this.alertMessage = errorMessage.error.message;
             }
         )
+    }
+
+    fileUploadEvent(fileInput: any) {
+        this.filesToUpload = <Array<File>>fileInput.target.files;
+        console.log(this.filesToUpload);
+    }
+
+    makeFileRequest(url: string, params: Array<string>, files: Array<File>) {
+        const token = this.token;
+
+        return new Promise((resolve, reject) => {
+            const formData: any = new FormData();
+            const xhr = new XMLHttpRequest();
+
+            for(let i=0; i<files.length; i++) {
+                formData.append('image', files[i], files[i].name)
+            }
+
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState == READY_STATE && xhr.status == HTTP_OK) {
+                    resolve(JSON.parse(xhr.response))
+                } else {
+                    reject(xhr.response);
+                }
+            };
+
+            xhr.open('POST', url, true);
+            xhr.setRequestHeader('Authorization', token)
+            xhr.send(formData);
+        });
     }
 
 }
